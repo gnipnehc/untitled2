e2c61361bef166d5350febbdc9c9350226a47791
shijiuyi <shijiuyi@socmap.net>
2020-07-01 14:51:29 +0800

    爬取ip的ip类型和ip归属地

---
+++ get_url_info/ip_info.py
@@ -0,0 +1,55 @@
+import re
+
+import numpy as np
+import pandas as pd
+import requests
+
+"""
+pip install requests
+pip install openpyxl
+pip install pandas
+pip install numpy
+pip install xlrd
+"""
+result = {}
+# 正则验证ip是否符合
+ipv4 = re.compile(r'^\d+\.\d+\.\d+\.\d+$')
+
+
+def get_ipinfo(ip):
+    if not ipv4.match(ip):
+        return
+    url = f'http://192.168.199.156:5027/service/offline?ip={ip}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()['data']
+        location = data['province'] + data['city']
+        ip_type = data['scene']
+        # 键值对保存数据
+        result[ip] = [location, ip_type]
+
+
+def main(filename):
+    # 打开表格
+    df = pd.read_excel(filename)
+    # 将NaN替换为空
+    df = df.replace(np.NaN, '')
+    df['IP节点'].map(get_ipinfo)
+    for ip, values in result.items():
+        df.loc[df['IP节点'] == ip, 'IP类型'] = values[1]
+        df.loc[df['IP节点'] == ip, 'IP归属地'] = values[0]
+    new_filename = '.'.join(filename.split('.')[:-1]) + '-new.xlsx'
+    df.to_excel(new_filename)
+
+
+if __name__ == '__main__':
+    file = '/home/shijiuyi/桌面/郑州网信办（IP）.xlsx'
+    main(file)
+
+"""
+1）行（列）选取（单维度选取）：df[]。这种情况一次只能选取行或者列，即一次选取中，只能为行或者列设置筛选条件（只能为一个维度设置筛选条件）。
+
+2）区域选取（多维选取）：df.loc[]，df.iloc[]，df.ix[]。这种方式可以同时为多个维度设置筛选条件。
+
+3）单元格选取（点选取）：df.at[]，df.iat[]。准确定位一个单元格。
+"""
