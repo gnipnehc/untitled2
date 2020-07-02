935bb84880a58cc4f3bcb25af5325b9a9d758ca6
shijiuyi <shijiuyi@socmap.net>
2020-07-02 16:09:55 +0800

    推送域名查询到icp

---
+++ get_url_info/icp.py
@@ -0,0 +1,19 @@
+import requests
+from tld import get_fld
+
+
+with open('domain.txt') as f:
+    for line in f.readlines():
+        try:
+            domain = get_fld(line.strip().strip())
+            url = 'http://10.125.0.26:10000/dispatch'
+            data = {
+                'domain': domain,
+                'refresh': True,
+                'source': "人为收录"
+            }
+
+            resp = requests.post(url, json=data)
+            print(resp.json())
+        except Exception as e:
+            print(e)
