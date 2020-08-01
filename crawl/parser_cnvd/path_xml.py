cbba8c6ef70b10e0d4ead7499ad4c1498a9b15c6
shijiuyi <shijiuyi@socmap.net>
2020-08-01 15:05:19 +0800

    xml文件的路径查询

---
+++ crawl/parser_cnvd/path_xml.py
@@ -0,0 +1,18 @@
+import os
+path = '/home/shijiuyi/Desktop/other_crawl/crawl_cnvd_list/xml_cnvd'
+list_name = []
+count = 0
+
+for file in os.listdir(path):
+    file_path = os.path.join(path, file)
+    if os.path.isdir(file_path):
+        os.listdir(file_path, list_name)
+    else:
+        list_name.append(file_path)
+
+# print(list_name)
+    with open('xml_name.txt', 'w') as f:
+        count += 1
+        for i in list_name:
+            f.write(i+'\n')
+        print('success: {}'.format(count))
